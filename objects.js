2f402846ef6d6747aab25578e6af35ae002edce6
Carleen mahon <carleenmahon92@gmail.com>
2018-08-08 00:23:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,7 @@
 
-<<<<<<< HEAD
+
 var playlist = {artistName: 'song' };
 
-=======
-//var playlist= artistName:”song”;
->>>>>>> 114f5e215e0355b2ebfeb5aca97d233bb5c8152a
 
 function updatePlaylist(playlist,artistName,song){
 playlist[artistName]=song
